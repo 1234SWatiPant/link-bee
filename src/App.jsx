28598ba7ab25abbc7661363871eaf10e7d51e57d
1234SWatiPant <swatipant44@gmail.com>
2024-08-10 18:35:23 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import React ,{lazy} from 'react'
+import Navbar from './Components/Navbar/Navbar';
+import Home from "./Components/Home/Home"; 
+import About from "./Components/About/About"; 
+import Services from "./Components/Services/Services"
+import Delivered from "./Components/Delivered/Delivered"; 
+import Login from "./Components/Login/Login";
+import Signup from "./Components/Signup/Signup";
+import Footer from './Components/Footer/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+const User = lazy (()=> import('./Components/User/User'));
+const FinalDisplay = lazy (()=> import('./Components/FinalDisplay/FinalDisplay'));
+const Message = lazy (()=> import('./Components/Message/Message'));
+const Error = lazy (()=> import('./Components/Error/Error'));
+
+export default function App() {
+
+  const currentUrl = window.location.pathname;
+  const parts = currentUrl.split('/');
+  const userID = parts[parts.length - 1];
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element=
+            {
+              <>
+                <Navbar />
+                <Home id="home" />
+                <About id="about" />
+                <Services id="services" />
+                <Delivered />
+                <Message />
+              </>
+            }>
+          </Route>
+          <Route element={<Login />} path='login' />
+          <Route element={<Signup />} path='signup' />
+          <Route element={<User />} path={`user/auth/edit/${userID}`} />
+          <Route element={<User />} path={`/unauth/tempUser`} />
+
+          <Route path={`user/${userID}`} element={
+            <>
+              <Navbar />
+              <Home id="home" />
+              <About id="about" />
+              <Services id="services" />
+              <Delivered />
+              <Message />
+            </>
+          }
+          />
+
+          <Route element={<FinalDisplay />} path={`${userID}`} />
+          <Route path="*" element={<Error/>}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter >
+    </>
+  )
+}
---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBZSIoUM3gWY0QwEJgm_c_SP3cuodLFe6k",
+  authDomain: "link-bee-18a8a.firebaseapp.com",
+  projectId: "link-bee-18a8a",
+  storageBucket: "link-bee-18a8a.appspot.com",
+  messagingSenderId: "755224115117",
+  appId: "1:755224115117:web:fdd46461866bba63bcd324",
+  measurementId: "G-EHR0H96VS8"
+};
+export default firebaseConfig;
